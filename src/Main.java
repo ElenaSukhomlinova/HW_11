9e1140b7b728b2f49d1283d0d8dc145262018d4a
ElenaSukhomlinova <fandeevaf2@gmail.com>
2024-12-07 22:32:33 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author bulychev = new Author("Kir","Bulychev");
+        Author pushkin = new Author("Alexandr", "Pushkin");
+
+        Book Alisa = new Book("Alisa v strane chudes", bulychev, 2024);
+        Book EvgeniyOnegin = new Book("Evgeniy Onegin", pushkin, 1986);
+
+        System.out.println(Alisa.getBookName() + " " + Alisa.getAuthor());
+        System.out.println(EvgeniyOnegin.getBookName() + " " + EvgeniyOnegin.getYear());
+
+        Alisa.setYear(2018);
+        System.out.println(Alisa.getBookName() + " " + Alisa.getYear());
     }
 }
